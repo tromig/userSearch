dd6ad5c3695c5fd878d7576d48c9056f150ece8e
Tony Romig <tony.romig@gmail.com>
2018-08-15 11:43:55 -0400

    moved state up to SearchFrom element

--- usersearch/src/SearchForm/SearchForm.js
+++ usersearch/src/SearchForm/SearchForm.js
@@ -5,19 +5,32 @@ import SearchInput from "./SearchInput/SearchInput";
 
 class SearchForm extends Component{
 
-	constructor(props){
-		super(props);
-
+	constructor(){
+		super();
 		this.state = {
-			userData: ''
+			userData: [],
+			showData: [],
 		};
 	}
+
+	componentDidMount(){
+		fetch('https://randomuser.me/api/?results=100')
+			.then(response => response.json())
+			.then(data => this.processData(data));
+	}
+
+	processData(data){
+		this.setState({
+			userData: data.results,
+			showData: data.results,
+		});
+	}	
 
 	render(){
 		return (
 			<div className='searchForm'>			    
 			    <SearchInput />
-			    <UserList users={this.state.userData} />
+			    <UserList userData={this.state.showData} />
 			</div>
 		);
 	}
--- usersearch/src/SearchForm/UserList/UserList.js
+++ usersearch/src/SearchForm/UserList/UserList.js
@@ -4,24 +4,7 @@ import "./UserList.css";
 
 
 class UserList extends Component {
-	constructor(){
-		super();
-		this.state = {
-			userData: []
-		};
-	}
-
-	componentDidMount(){
-		fetch('https://randomuser.me/api/?results=100')
-			.then(response => response.json())
-			.then(data => this.processData(data));
-	}
-
-	processData(data){
-		this.setState({
-			userData: data.results
-		});
-	}
+	
 
 	formatDate(dateString){
 		let workDate = dateString.split("T")[0];
@@ -37,9 +20,9 @@ class UserList extends Component {
 	formatLoc(user){
 		return user.location.street + ", " + user.location.city + ", " + user.location.state
 	}
-
+	
 	render(){
-		const userItems = this.state.userData.map((user) => (
+		const userItems = this.props.userData.map((user) => (
 															<UserItem thumb={user.picture.thumbnail} 
 																	  name={this.formatName(user)}	
 																	  age={user.dob.age}
